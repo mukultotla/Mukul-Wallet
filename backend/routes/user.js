4489773ecec5592ed478b997aad722503e6c59b8
Mukul <mukultotla4@gmail.com>
2024-03-04 23:39:29 +0530

    routes

---
+++ backend/routes/user.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { signUp, signIn } = require("../controllers/user");
+const router = express.Router();
+
+router.post("/signin", signIn);
+router.post("/signup", signUp);
+
+module.exports = router;
