2d7d29175b9a6a49cfda01481d5ccba567690964
Mukul <mukultotla4@gmail.com>
2024-03-05 19:54:39 +0530

    updated conditional check in auth middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,13 +2,16 @@ const jwt = require("jsonwebtoken");
 
 const authMiddleware = async (req, res, next) => {
   try {
-    if (req.headers.authorization) {
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer ")
+    ) {
       const token = req.headers.authorization.split(" ")[1];
       const payload = await jwt.verify(token, process.env.JWT_SECRET);
-      req.user = {userId: payload.userId};
+      req.userId = payload.userId;
       console.log(req.user);
     } else {
-        throw new Error("Missing required auth headers");
+      throw new Error("Missing required auth headers");
     }
     next();
   } catch (error) {
